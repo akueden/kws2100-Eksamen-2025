edf56f607a0888028157250c9a01f74f640b8b14
Emma <emjo019@student.kristiania.no>
2025-05-06 00:37:57 +0200

    alle har tilgang og prosjektet funker

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,3 @@ import { createRoot } from "react-dom/client";
 import { Application } from "./modules/app/application";
 
 createRoot(document.getElementById("root")!).render(<Application />);
-
-//Skriv kommentar + navn her:
-//Heisann - Anna
-// Hei, hvordan går det - Amima
